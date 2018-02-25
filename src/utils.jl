5fcc961ccf23945d5f0b6e5b4635443230a61bf9
piever <pietro.vertechi@neuro.fchampalimaud.org>
2018-02-25 19:35:31 +0000

    get dot exception right

--- src/utils.jl
+++ src/utils.jl
@@ -2,8 +2,11 @@ parse_function_call(args...) = parse_function_call!([], args...)
 
 parse_function_call!(syms, d, x, func, args...) = x == :(_) ? d : x
 
+isquotenode(x) = false
+isquotenode(x::Expr) = x.head == :quote
+
 function parse_function_call!(syms, d, x::Expr, func, args...)
-    if x.head == :. && length(x.args) == 2
+    if x.head == :. && length(x.args) == 2 && isquotenode(x.args[2])
         return Expr(x.head, parse_function_call!(syms, d, x.args[1], func, args...), x.args[2])
     end
     if x.head == :quote
