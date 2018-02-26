f11e199da1973bb91d96c7ad7115f11cf67e05b9
piever <pietro.vertechi@neuro.fchampalimaud.org>
2018-02-26 10:46:39 +0000

    add escaping

--- src/utils.jl
+++ src/utils.jl
@@ -7,13 +7,15 @@ isquotenode(x::Expr) = x.head == :quote
 
 function parse_function_call!(syms, d, x::Expr, func, args...)
     if x.head == :. && length(x.args) == 2 && isquotenode(x.args[2])
-        return Expr(x.head, parse_function_call!(syms, d, x.args[1], func, args...), x.args[2])
-    end
-    if x.head == :quote
+        Expr(x.head, parse_function_call!(syms, d, x.args[1], func, args...), x.args[2])
+    elseif x.head == :quote
         push!(syms, x)
-        return func(d, x, args...)
+        func(d, x, args...)
+    elseif x.head == :call && length(x.args) == 2 && x.args[1] == :^
+        x.args[2]
+    else
+        Expr(x.head, (parse_function_call!(syms, d, arg, func, args...) for arg in x.args)...)
     end
-    return Expr(x.head, (parse_function_call!(syms, d, arg, func, args...) for arg in x.args)...)
 end
 
 function extract_anonymous_function(d, x, func)
