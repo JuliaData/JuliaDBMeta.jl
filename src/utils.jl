a8f08352bf98faafde4e722f14a987a11869c21c
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-04-28 18:06:44 +0100

    Allow white space after curly brackets

--- src/utils.jl
+++ src/utils.jl
@@ -63,27 +63,25 @@ end
 
 # From Query: use curly brackets to simplify writing named tuples
 function helper_namedtuples_replacement(ex)
-	MacroTools.postwalk(ex) do x
-		if x isa Expr && x.head==:braces
-			new_ex = Expr(:tuple, x.args...)
-
-			for (j,field_in_NT) in enumerate(new_ex.args)
-				if isa(field_in_NT, Expr) && field_in_NT.head==:(=)
-					new_ex.args[j] = Expr(:(=), field_in_NT.args...)
-				elseif isquotenode(field_in_NT)
-					new_ex.args[j] = Expr(:(=), field_in_NT.value, field_in_NT)
+    MacroTools.postwalk(ex) do x
+        if x isa Expr && x.head in (:braces, :bracescat)
+            new_ex = Expr(:tuple, x.args...)
+            for (j,field_in_NT) in enumerate(new_ex.args)
+                if isa(field_in_NT, Expr) && field_in_NT.head==:(=)
+                    new_ex.args[j] = Expr(:(=), field_in_NT.args...)
+                elseif isquotenode(field_in_NT)
+                    new_ex.args[j] = Expr(:(=), field_in_NT.value, field_in_NT)
                 elseif isa(field_in_NT, Expr)
                     new_ex.args[j] = Expr(:(=), Symbol(filter(t -> t != ':', string(field_in_NT))), field_in_NT)
                 elseif isa(field_in_NT, Symbol)
-                   new_ex.args[j] = Expr(:(=), field_in_NT, field_in_NT)
+                    new_ex.args[j] = Expr(:(=), field_in_NT, field_in_NT)
                 end
-			end
-
-			return new_ex
-		else
-			return x
-		end
-	end
+            end
+            return new_ex
+        else
+            return x
+        end
+    end
 end
 
 replace_keyword(arg) = (@capture arg x_ = y_) ? Expr(:kw, x, y) : arg
